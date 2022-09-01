7f3a7a3f0045e77ae46b921f6bb213061987f22b
mahahahajan <pulkit8.mahajan@gmail.com>
2022-08-31 23:16:32 -0700

    idk if this worked

---
+++ README.md
@@ -0,0 +1,4 @@
+# TodayApp
+Creating a Flutter app to act as my morning hub (weather, outfit, water, and todos)
+
+Test test
--- lib/components/header_component.dart
+++ lib/components/header_component.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HeaderComponent extends StatelessWidget {
-  
   String currTime = "";
 
   HeaderComponent({Key? key}) : super(key: key);
 
-  String getTimeOfDay (int time){
-    if(3 < time && time < 12){
+  String getTimeOfDay(int time) {
+    if (3 < time && time < 12) {
       return 'Morning';
-    } else if (12 < time && time < 16){
+    } else if (12 < time && time < 16) {
       return 'Afternoon';
     } else if (16 < time && time < 19) {
       return 'Evening';
@@ -22,26 +21,24 @@ class HeaderComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     currTime = getTimeOfDay(17);
     print(currTime);
-    return Row(
-      children: <Widget>[
-        Expanded(
+    return Row(children: <Widget>[
+      Expanded(
           child: Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
-            child: Text( 
-              'Good $currTime Pulkit', 
-              textScaleFactor: 2,
-              style: TextStyle(fontWeight: FontWeight.w400),
-            ),
-          )
+        padding: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+        child: Text(
+          'Good $currTime Pulkit',
+          textScaleFactor: 2,
+          style: TextStyle(fontWeight: FontWeight.w400),
         ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 20.0, 0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/9956077?v=4"),
-            radius: 25.0,
-          ),
-        )
-      ]
-    );
+      )),
+      Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 20.0, 0),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://avatars.githubusercontent.com/u/9956077?v=4"),
+          radius: 25.0,
+        ),
+      )
+    ]);
   }
 }
