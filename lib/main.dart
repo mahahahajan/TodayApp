cba15f5ff1f26673cc574e8f489073ee88d0ee15
mahahahajan <pulkit8.mahajan@gmail.com>
2022-09-21 00:06:14 -0700

    lazy day, so just updated the icons, added something to help me test on more devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:today_app/screens/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:today_app/constants.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const TodayApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => TodayApp(), // Wrap your app
+    ),
+  );
 }
 
 class TodayApp extends StatelessWidget {
@@ -15,6 +22,9 @@ class TodayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Today App - Today at a Glance',
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
         //Use dark theme, change some things
